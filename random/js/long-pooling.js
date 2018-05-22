7364192e9cdfefc91315fa8af98cc67dfb231091
Stormfluger <turkom76@mail.ru>
2018-05-22 14:38:40 +0500

    work

--- random/js/long-pooling.js
+++ random/js/long-pooling.js
@@ -1,1 +1,20 @@
 'use strict';
+
+sendLongRequest();
+
+function sendLongRequest() {
+    fetch('https://neto-api.herokuapp.com/comet/long-pooling')
+        .then(res => res.json())
+        .then(number => {
+            showNumber('long-pooling', number);
+            sendLongRequest();
+        })
+        .catch(err => console.log(err));
+}
+
+function showNumber(linkType, number) {
+    const numberTreatment = Math.round(+number) % 11;
+    Array.from(document.getElementsByClassName(linkType)[0].children)
+        .forEach(elem => elem.classList.remove('flip-it'));
+document.getElementsByClassName(linkType)[0].children[numberTreatment].classList.add('flip-it');
+}
--- random/js/pooling.js
+++ random/js/pooling.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const timer = setInterval(sendShortRequest, 8000);
+
+function sendShortRequest() {
+    fetch('https://neto-api.herokuapp.com/comet/pooling')
+        .then(res => res.json())
+        .then(number => {
+            showNumber('pooling', number);
+        })
+        .catch(err => console.log(err));
+}
